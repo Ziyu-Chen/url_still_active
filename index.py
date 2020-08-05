ab9f3aabe0dd0a05855b7128d34ae7aa067a105f
Ziyu-Chen <zc839@nyu.edu>
2020-08-05 02:14:10 -0400

    Made some minor changes.

--- index.py
+++ index.py
@@ -2,28 +2,43 @@ import pandas as pd
 from generate_indicator import generate_indicator
 from generate_stopper import generate_stopper
 from get_html import get_html
+from requests.exceptions import ConnectionError
+from requests.exceptions import ChunkedEncodingError
 import time
 
 website = 'lazada'
 file_path = '/Users/ziyu/Desktop/lego/listings.csv'
 column = 'Listing URL'
-sleep_time = 10
+sleep_time = 5
 
 indicator = generate_indicator(website)
 stopper = generate_stopper(website)
 data = pd.read_csv(file_path)
-for i in range(len(data)):
+i = 0
+while i < len(data):
     if str(data.iloc[i][column + ' still active']) != 'nan':
+        i += 1
         continue
     url = data.iloc[i][column]
-    html = get_html(url)
-    if stopper in html:
+    try:
+        html = get_html(url)
+        if stopper in html:
+            print('The anti-webscrapping mechanism of this website is on.')
+            break
+        has_indicator = indicator in html
+        data.at[i, column + ' still active'] = 1 if has_indicator else 0
+        if i % 10 == 9:
+            data.to_csv(file_path, encoding='utf-8')
+        print('Finished No. %d' % i)
+        i += 1
+        time.sleep(sleep_time)
+    except ConnectionError:
+        time.sleep(sleep_time)
+        continue
+    except ChunkedEncodingError:
+        time.sleep(sleep_time)
+        continue
+    except Exception:
         break
-    has_indicator = indicator in html
-    data.at[i, column + ' still active'] = 1 if has_indicator else 0
-    if i % 10 == 9:
-        data.to_csv(file_path, encoding='utf-8')
-    print('Finished No. %d' % i)
-    time.sleep(sleep_time)
 
 data.to_csv(file_path, encoding='utf-8')
